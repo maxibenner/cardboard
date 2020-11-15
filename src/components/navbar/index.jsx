ea5ecc6e29ba1eb90f6ff9e2d9cf39abc7e9bec9
Maximilian Benner <maxibenner@gmail.com>
2020-11-15 16:26:14 +0100

    remove unused import

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../../media/logo-dark.svg"
 import ArrowText from "../arrow-text"
