dc2169d4aa356d10378beb12275c12d09c4d8c7a
Maximilian Benner <maxibenner@gmail.com>
2020-12-28 13:43:00 +0100

    sync

--- src/components/cardFile/index.jsx
+++ src/components/cardFile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import styles from "./styles.module.css";
 import ToggleContext from '../toggleContext';
 import Dropdown from '../dropdown';
@@ -24,18 +24,20 @@ export default function CardFile(props) {
 
     // counter > 0 = is hovered
     const [dragCounter, setDragCounter] = useState(0)
-    
+
 
 
 
     //_________________ FUNCTIONS _________________//
-    
+
     // Handle file delete
-    const handleDelete = (e) => {
-        firebase.firestore().collection('users').doc(props.file.owner).collection('files').doc(props.file.id).delete().then(() => {
-            console.info('Deleted')
-        }).catch((err) => console.err(err))
-    }
+    const handleDelete = useCallback(
+        (e) => {
+            firebase.firestore().collection('users').doc(props.file.owner).collection('files').doc(props.file.id).delete().then(() => {
+                console.info('Deleted')
+            }).catch((err) => console.err(err))
+        }, [props.file.id, props.file.owner],
+    )
 
     // Prevent default if necessary
     const preventDefault = (e) => {
@@ -44,40 +46,48 @@ export default function CardFile(props) {
     }
 
     // Handle rename
-    const handleRename = (e) => {
-        e.stopPropagation()
-        setMenuActive(false)
-        setInputActive(true)
-    }
+    const handleRename = useCallback(
+        (e) => {
+            e.stopPropagation()
+            setMenuActive(false)
+            setInputActive(true)
+        }, [],
+    )
 
     // Handle change
-    const handleChange = () => {
-        setTitle(input.current.value)
-    }
+    const handleChange = useCallback(
+        () => {
+            setTitle(input.current.value)
+        }, [],
+    )
 
     // Handle focus loss
-    const handleFocusLoss = (e) => {
-        e.stopPropagation()
-        setInputActive(false)
-        firebase.firestore().collection('users').doc(props.file.owner).collection('files').doc(props.file.id).update({ name: title })
-            .then(() => {
-                console.info('Updated title')
-            }).catch((err) => console.error(err))
-    }
-
-    // Handle title submit
-    const handleKeyPress = (e) => {
-        console.log('key')
-        if (e.code === "Enter") {
-            e.preventDefault();
-
+    const handleFocusLoss = useCallback(
+        (e) => {
+            e.stopPropagation()
             setInputActive(false)
             firebase.firestore().collection('users').doc(props.file.owner).collection('files').doc(props.file.id).update({ name: title })
                 .then(() => {
-                    console.info('Submitted title')
+                    console.info('Updated title')
                 }).catch((err) => console.error(err))
-        }
-    }
+        }, [props.file.owner, props.file.id, title],
+    )
+
+    // Handle title submit
+    const handleKeyPress = useCallback(
+        (e) => {
+            console.log('key')
+            if (e.code === "Enter") {
+                e.preventDefault();
+
+                setInputActive(false)
+                firebase.firestore().collection('users').doc(props.file.owner).collection('files').doc(props.file.id).update({ name: title })
+                    .then(() => {
+                        console.info('Submitted title')
+                    }).catch((err) => console.error(err))
+            }
+        }, [props.file.id, props.file.owner, title],
+    )
 
     // Set input focus
     useEffect(() => {
@@ -90,51 +100,67 @@ export default function CardFile(props) {
 
     //_____________________________DRAGGIN___________________________//
     //Handle drag start
-    const onDragStartFunctions = () => {
-        props.onDragStart(props.file.id)
-        setIsDragged(true)
-    }
+    const onDragStartFunctions = useCallback(
+        () => {
+            props.onDragStart(props.file.id)
+            setIsDragged(true)
+        }, [props],
+    )
+
     // Handle drag enter
-    const handleDragEnter = (e) => {
-        // Only set as target if not equal to source
-        if (!isDragged) {
-            setDragCounter(dragCounter => dragCounter + 1)
-        }
-    }
+    const handleDragEnter = useCallback(
+        (e) => {
+            // Only set as target if not equal to source
+            if (!isDragged) {
+                setDragCounter(1)
+            }
+        }, [isDragged],
+    )
+
     //Handle drag end
-    const handleDragEnd = (e) => {
-        e.preventDefault()
-        setIsDragged(false)
-    }
+    const handleDragEnd = useCallback(
+        (e) => {
+            e.preventDefault()
+            setIsDragged(false)
+        }, [],
+    )
+
     // Handle drag exit
-    const handleDragLeave = () => {
-        // Only remove as target if not equal to source
-        if (!isDragged) {
-            setDragCounter(dragCounter => dragCounter - 1)
-        }
-    }
+    const handleDragLeave = useCallback(
+        () => {
+            // Only remove as target if not equal to source
+            if (!isDragged) {
+                setDragCounter(0)
+            }
+        }, [isDragged],
+    )
+
     // Handle drag over
-    const handleDragOver = (e) => {
-        e.preventDefault()
-    }
+    const handleDragOver = useCallback(
+        (e) => {
+            e.preventDefault()
+        }, [],
+    )
+
     // Handle drag drop
-    const onDragDropFunctions = (e) => {
-        setDragCounter(0)
-        // Only trigger when target if not equal to source
-        if (!isDragged) {
-            props.onDrop({
-                id: props.file.id,
-                display_type: 'file'
-            })
-        }
-    }
+    const onDragDropFunctions = useCallback(
+        (e) => {
+            setDragCounter(0)
+            // Only trigger when target if not equal to source
+            if (!isDragged) {
+                props.onDrop({
+                    id: props.file.id,
+                    display_type: 'file'
+                })
+            }
+        }, [isDragged, props],
+    )
 
 
 
     return (
         <div
             className={`${styles.card} ${dragCounter !== 0 && styles.is_hovered} ${isDragged && styles.is_dragged}`}
-            test={console.log('render')}
             draggable={draggable}
             onDragStart={onDragStartFunctions}
             onDragEnter={handleDragEnter}
@@ -143,19 +169,20 @@ export default function CardFile(props) {
             onDragLeave={handleDragLeave}
             onDrop={onDragDropFunctions}
         >
-            <div className={styles.cardInner}>
+            <div className={`${styles.cardInner} ${dragCounter !== 0 && styles.no_click}`}>
                 <div className={styles.videoContainer} onClick={() => props.handleActiveMedia(props.file, 'show')}>
-                    {props.file.thumbnail_url && props.file.type === 'video' &&
-                        <MdPlayCircleFilled className={styles.playButton} />
-                    }
                     {!props.file.thumbnail_url && props.file.type === 'image' &&
                         <MdImage className={styles.processingButton} />
                     }
                     {!props.file.thumbnail_url && props.file.type === 'video' &&
                         <FaVideo className={styles.processingButton} />
                     }
+                    {props.file.thumbnail_url && props.file.type === 'video' &&
+                        <MdPlayCircleFilled className={styles.playButton} />
+                    }
                     <div className={styles.image} style={props.file.thumbnail_url && { backgroundImage: `url(${props.file.thumbnail_url})` }}></div>
                 </div>
+                
                 <div className={styles.body}>
                     <div className={styles.main}>
 
