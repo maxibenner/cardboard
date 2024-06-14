e454d4b0dfcf78b479be1fe96175677b33dfb36c
Maximilian Benner <maxibenner@gmail.com>
2024-06-13 20:50:31 -0400

    fix: added firebase-functions/v1

--- functions/lib/init.js
+++ functions/lib/init.js
@@ -1,5 +1,5 @@
 // Firebase
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
